354e38caf71357fedf87c3e80fbb901ac772e960
ijlee2 <ijlee2@users.noreply.github.com>
2023-07-26 21:21:23 +0200

    wip: Transformed code

--- src/steps/rename-integration-tests.ts
+++ src/steps/rename-integration-tests.ts
@@ -1,12 +1,64 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { readFileSync, writeFileSync } from 'node:fs';
 import { join } from 'node:path';
 
-import { findFiles } from '@codemod-utils/files';
+import { AST } from '@codemod-utils/ast-javascript';
+import { findFiles, parseFilePath } from '@codemod-utils/files';
 
 import type { Options } from '../types/index.js';
 
-function renameModule(file: string): string {
-  return file;
+type Data = {
+  isTypeScript: boolean;
+  moduleName: string;
+};
+
+function getModuleName(filePath: string): string {
+  let { dir, name } = parseFilePath(filePath);
+
+  dir = dir.replace(/^tests\/integration(\/)?/, '');
+  name = name.replace(/-test$/, '');
+
+  const entityName = join(dir, name);
+
+  // a.k.a. friendlyTestDescription
+  return ['Integration', entityName].join(' | ');
+}
+
+function renameModule(file: string, data: Data): string {
+  const traverse = AST.traverse(data.isTypeScript);
+
+  const ast = traverse(file, {
+    visitCallExpression(node) {
+      if (
+        node.value.callee.type !== 'Identifier' ||
+        node.value.callee.name !== 'module'
+      ) {
+        return false;
+      }
+
+      if (node.value.arguments.length !== 2) {
+        return false;
+      }
+
+      switch (node.value.arguments[0].type) {
+        case 'Literal': {
+          node.value.arguments[0] = AST.builders.literal(data.moduleName);
+
+          break;
+        }
+
+        case 'StringLiteral': {
+          node.value.arguments[0] = AST.builders.stringLiteral(data.moduleName);
+
+          break;
+        }
+      }
+
+      return false;
+    },
+  });
+
+  return AST.print(ast);
 }
 
 export function renameIntegrationTests(options: Options): void {
@@ -20,7 +72,12 @@ export function renameIntegrationTests(options: Options): void {
     const oldPath = join(projectRoot, filePath);
     const oldFile = readFileSync(oldPath, 'utf8');
 
-    const newFile = renameModule(oldFile);
+    const data = {
+      isTypeScript: filePath.endsWith('.ts'),
+      moduleName: getModuleName(filePath),
+    };
+
+    const newFile = renameModule(oldFile, data);
 
     writeFileSync(oldPath, newFile, 'utf8');
   });
