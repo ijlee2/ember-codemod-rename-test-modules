f4f67b7bd8200f136a40b53faa2959f6ccd10fd7
ijlee2 <ijlee2@users.noreply.github.com>
2023-07-26 08:32:08 +0200

    wip: Found files

--- src/steps/rename-acceptance-tests.ts
+++ src/steps/rename-acceptance-tests.ts
@@ -1,5 +1,13 @@
+import { findFiles } from '@codemod-utils/files';
+
 import type { Options } from '../types/index.js';
 
 export function renameAcceptanceTests(options: Options): void {
-  // ...
+  const { projectRoot } = options;
+
+  const filePaths = findFiles('tests/acceptance/**/*-test.{js,ts}', {
+    projectRoot,
+  });
+
+  console.log(filePaths);
 }
