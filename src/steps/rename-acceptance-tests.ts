6868aac71d155c0661313365e5f37cdef11d0ded
ijlee2 <ijlee2@users.noreply.github.com>
2023-07-26 10:14:44 +0200

    wip: Traversed file

--- src/steps/rename-acceptance-tests.ts
+++ src/steps/rename-acceptance-tests.ts
@@ -1,12 +1,23 @@
 import { readFileSync, writeFileSync } from 'node:fs';
 import { join } from 'node:path';
 
+import { AST } from '@codemod-utils/ast-javascript';
 import { findFiles } from '@codemod-utils/files';
 
 import type { Options } from '../types/index.js';
 
-function renameModule(file: string): string {
-  return file;
+type Data = {
+  isTypeScript: boolean;
+};
+
+function renameModule(file: string, data: Data): string {
+  const traverse = AST.traverse(data.isTypeScript);
+
+  const ast = traverse(file, {
+    // ...
+  });
+
+  return AST.print(ast);
 }
 
 export function renameAcceptanceTests(options: Options): void {
@@ -20,7 +31,11 @@ export function renameAcceptanceTests(options: Options): void {
     const oldPath = join(projectRoot, filePath);
     const oldFile = readFileSync(oldPath, 'utf8');
 
-    const newFile = renameModule(oldFile);
+    const data = {
+      isTypeScript: filePath.endsWith('.ts'),
+    };
+
+    const newFile = renameModule(oldFile, data);
 
     writeFileSync(oldPath, newFile, 'utf8');
   });
