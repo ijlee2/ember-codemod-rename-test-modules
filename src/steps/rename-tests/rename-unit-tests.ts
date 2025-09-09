22472d9226d42a5e7459bf05db0ca9a2771c0fce
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-09-09 08:47:27 +0200

    feature: Supported *.{gjs,gts} files

--- src/steps/rename-tests/rename-unit-tests.ts
+++ src/steps/rename-tests/rename-unit-tests.ts
@@ -1,10 +1,15 @@
 import { readFileSync, writeFileSync } from 'node:fs';
 import { join } from 'node:path';
 
+import { updateJavaScript } from '@codemod-utils/ast-template-tag';
 import { findFiles, parseFilePath } from '@codemod-utils/files';
 
 import type { Options } from '../../types/index.js';
-import { parseEntity, renameModule } from '../../utils/rename-tests/index.js';
+import {
+  isTypeScript,
+  parseEntity,
+  renameModule,
+} from '../../utils/rename-tests/index.js';
 
 const folderToEntityType = new Map([
   ['adapters', 'Adapter'],
@@ -35,20 +40,26 @@ function getModuleName(filePath: string): string {
 export function renameUnitTests(options: Options): void {
   const { projectRoot } = options;
 
-  const filePaths = findFiles('tests/unit/**/*-test.{js,ts}', {
+  const filePaths = findFiles('tests/unit/**/*-test.{gjs,gts,js,ts}', {
     projectRoot,
   });
 
   filePaths.forEach((filePath) => {
     const oldPath = join(projectRoot, filePath);
-    const oldFile = readFileSync(oldPath, 'utf8');
+    let newFile = readFileSync(oldPath, 'utf8');
 
     const data = {
-      isTypeScript: filePath.endsWith('.ts'),
+      isTypeScript: isTypeScript(filePath),
       moduleName: getModuleName(filePath),
     };
 
-    const newFile = renameModule(oldFile, data);
+    if (filePath.endsWith('.js') || filePath.endsWith('.ts')) {
+      newFile = renameModule(newFile, data);
+    } else {
+      newFile = updateJavaScript(newFile, (code) => {
+        return renameModule(code, data);
+      });
+    }
 
     writeFileSync(oldPath, newFile, 'utf8');
   });
