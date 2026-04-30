002314b8cc78340bdf646d669e1f871870a50e9c
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2026-04-30 16:37:03 +0200

    chore: Fixed lint errors

--- src/utils/rename-tests/rename-module.ts
+++ src/utils/rename-tests/rename-module.ts
@@ -9,34 +9,26 @@ export function renameModule(file: string, data: Data): string {
   const traverse = AST.traverse(data.isTypeScript);
 
   const ast = traverse(file, {
-    visitCallExpression(node) {
+    visitCallExpression(path) {
       if (
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-        node.value.callee.type !== 'Identifier' ||
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-        node.value.callee.name !== 'module'
+        path.node.callee.type !== 'Identifier' ||
+        path.node.callee.name !== 'module'
       ) {
         return false;
       }
 
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-      if (node.value.arguments.length !== 2) {
+      if (path.node.arguments.length !== 2) {
         return false;
       }
 
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-      switch (node.value.arguments[0].type) {
+      switch (path.node.arguments[0]!.type) {
         case 'Literal': {
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-          node.value.arguments[0] = AST.builders.literal(data.moduleName);
-
+          path.node.arguments[0] = AST.builders.literal(data.moduleName);
           break;
         }
 
         case 'StringLiteral': {
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-          node.value.arguments[0] = AST.builders.stringLiteral(data.moduleName);
-
+          path.node.arguments[0] = AST.builders.stringLiteral(data.moduleName);
           break;
         }
       }
